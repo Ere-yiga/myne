9a19ec9ef220786725e2687dfad0360ccdf47784
Ere-yiga <erehdagr8@gmail.com>
2026-04-06 22:06:43 +0100

    update

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -5,6 +5,34 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text("PickVault",
+          style: TextStyle(
+            fontStyle: FontStyle.italic,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          )
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Image.asset('lib/images/bts.jpg', height: 240),
+            ),
+            
+            const SizedBox(height: 40),
+            Text("Spikey", style: TextStyle(fontSize: 24,)),
+            const SizedBox(height: 40),
+            Text("Brand new dark theme image, made with custom theme image, just for you.", 
+            style: TextStyle(fontSize: 14, color: Colors.grey,), textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+    );
   }
 }
