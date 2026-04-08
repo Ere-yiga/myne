e59d2fab2e78a0ff613da56f81ada3bed72e203a
Ere-yiga <erehdagr8@gmail.com>
2026-04-08 08:51:55 +0100

    update

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -7,12 +7,13 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text("PickVault",
+        title: Text(
+          "PickVault",
           style: TextStyle(
             fontStyle: FontStyle.italic,
             fontSize: 24,
             fontWeight: FontWeight.bold,
-          )
+          ),
         ),
         centerTitle: true,
         backgroundColor: Colors.amberAccent,
@@ -24,21 +25,31 @@ class IntroPage extends StatelessWidget {
               padding: const EdgeInsets.all(25.0),
               child: Image.asset('lib/images/bts.jpg', height: 240),
             ),
-            
+
             const SizedBox(height: 40),
-            Text("Spikey", style: TextStyle(fontSize: 24,)),
+
+            Text("Spikey", style: TextStyle(fontSize: 24)),
+
             const SizedBox(height: 40),
-            Text("Brand new dark theme image, made with custom theme image, just for you.", 
-            style: TextStyle(fontSize: 14, color: Colors.grey,), textAlign: TextAlign.center),
+
+            Text(
+              "Brand new dark theme image, made with custom theme image, just for you.",
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
           ],
         ),
       ),
-      
+
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart'
           ),
         ],
       ),
